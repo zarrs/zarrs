1acadea68089066f26ac00945867aa646ac621b0
Lachlan Deakin <ljdgit@gmail.com>
2025-06-28 10:13:33 +1000

    test

--- zarrs/tests/array_partial_encode.rs
+++ zarrs/tests/array_partial_encode.rs
@@ -214,13 +214,6 @@ fn array_partial_encode_sharding_index_end() {
     array_partial_encode_sharding(ShardingIndexLocation::End, vec![]).unwrap();
 }
 
-#[cfg(all(
-    feature = "gzip",
-    feature = "bz2",
-    feature = "blosc",
-    feature = "crc32c",
-    feature = "zstd"
-))]
 #[test]
 fn array_partial_encode_sharding_index_compressed() {
     use zarrs_metadata_ext::codec::blosc::{
@@ -232,14 +225,18 @@ fn array_partial_encode_sharding_index_compressed() {
         array_partial_encode_sharding(
             *index_location,
             vec![
+                #[cfg(feature = "gzip")]
                 Arc::new(zarrs::array::codec::GzipCodec::new(5).unwrap()),
+                #[cfg(feature = "zstd")]
                 Arc::new(zarrs::array::codec::ZstdCodec::new(
                     5.try_into().unwrap(),
                     true,
                 )),
+                #[cfg(feature = "bz2")]
                 Arc::new(zarrs::array::codec::Bz2Codec::new(
                     Bz2CompressionLevel::try_from(5u8).unwrap(),
                 )),
+                #[cfg(feature = "blosc")]
                 Arc::new(
                     zarrs::array::codec::BloscCodec::new(
                         BloscCompressor::BloscLZ,
@@ -250,7 +247,10 @@ fn array_partial_encode_sharding_index_compressed() {
                     )
                     .unwrap(),
                 ),
+                #[cfg(feature = "crc32c")]
                 Arc::new(zarrs::array::codec::Crc32cCodec::new()),
+                #[cfg(feature = "adler32")]
+                Arc::new(zarrs::array::codec::Adler32Codec::default()),
             ],
         )
         .unwrap();
