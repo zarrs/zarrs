zarrs/zarrs

commit 49ee7047892dd31646173404f159956a4620110f

refactor!: improve `ArrayBuilder` and `{Array,Chunk}Representation` API (#214)

zarrs/benches/array_blosc.rs
@@ -17,8 +17,8 @@ fn array_blosc_write_all(c: &mut Criterion) {
                 let array = zarrs::array::ArrayBuilder::new(
                     vec![size; 3],
                     zarrs::array::DataType::UInt8,
-                    vec![32; 3].try_into().unwrap(),
-                    zarrs::array::FillValue::from(0u8),
+                    vec![32; 3],
+                    0u8,
                 )
                 .bytes_to_bytes_codecs(vec![Arc::new(
                     BloscCodec::new(
@@ -52,8 +52,8 @@ fn array_blosc_read_all(c: &mut Criterion) {
             let array = zarrs::array::ArrayBuilder::new(
                 vec![size; 3],
                 zarrs::array::DataType::UInt8,
-                vec![32; 3].try_into().unwrap(),
-                zarrs::array::FillValue::from(0u8),
+                vec![32; 3],
+                0u8,
             )
             .bytes_to_bytes_codecs(vec![Arc::new(
                 BloscCodec::new(

zarrs/benches/array_uncompressed.rs
@@ -14,8 +14,8 @@ fn array_write_all(c: &mut Criterion) {
                 let array = zarrs::array::ArrayBuilder::new(
                     vec![size; 3],
                     zarrs::array::DataType::UInt8,
-                    vec![32; 3].try_into().unwrap(),
-                    zarrs::array::FillValue::from(0u8),
+                    vec![32; 3],
+                    0u8,
                 )
                 .build(store.into(), "/")
                 .unwrap();
@@ -41,8 +41,8 @@ fn array_write_all_sharded(c: &mut Criterion) {
                 let array = zarrs::array::ArrayBuilder::new(
                     vec![size; 3],
                     zarrs::array::DataType::UInt16,
-                    vec![size; 3].try_into().unwrap(),
-                    zarrs::array::FillValue::from(0u16),
+                    vec![size; 3],
+                    0u16,
                 )
                 .array_to_bytes_codec(sharding_codec)
                 .build(store.into(), "/")
@@ -67,8 +67,8 @@ fn array_read_all(c: &mut Criterion) {
             let array = zarrs::array::ArrayBuilder::new(
                 vec![size; 3],
                 zarrs::array::DataType::UInt16,
-                vec![32; 3].try_into().unwrap(),
-                zarrs::array::FillValue::from(0u16),
+                vec![32; 3],
+                0u16,
             )
             .build(store.into(), "/")
             .unwrap();
@@ -98,8 +98,8 @@ fn array_read_all_sharded(c: &mut Criterion) {
             let array = zarrs::array::ArrayBuilder::new(
                 vec![size; 3],
                 zarrs::array::DataType::UInt8,
-                vec![size; 3].try_into().unwrap(),
-                zarrs::array::FillValue::from(1u8),
+                vec![size; 3],
+                1u8,
             )
             .array_to_bytes_codec(sharding_codec)
             .build(store.into(), "/")

zarrs/benches/codecs.rs
@@ -29,7 +29,7 @@ fn codec_bytes(c: &mut Criterion) {
         let rep = ChunkRepresentation::new(
             vec![num_elements.try_into().unwrap(); 1],
             DataType::UInt16,
-            0u16.into(),
+            0u16,
         )
         .unwrap();
 

zarrs/examples/array_write_read.rs
@@ -8,7 +8,7 @@ use zarrs::storage::{
 fn array_write_read() -> Result<(), Box<dyn std::error::Error>> {
     use std::sync::Arc;
     use zarrs::{
-        array::{DataType, FillValue, ZARR_NAN_F32},
+        array::{DataType, ZARR_NAN_F32},
         array_subset::ArraySubset,
         node::Node,
         storage::store,
@@ -58,8 +58,8 @@ fn array_write_read() -> Result<(), Box<dyn std::error::Error>> {
     let array = zarrs::array::ArrayBuilder::new(
         vec![8, 8], // array shape
         DataType::Float32,
-        vec![4, 4].try_into()?, // regular chunk shape
-        FillValue::from(ZARR_NAN_F32),
+        vec![4, 4], // regular chunk shape
+        ZARR_NAN_F32,
     )
     // .bytes_to_bytes_codecs(vec![]) // uncompressed
     .dimension_names(["y", "x"].into())

zarrs/examples/array_write_read_ndarray.rs
@@ -9,7 +9,7 @@ use zarrs::storage::{
 fn array_write_read() -> Result<(), Box<dyn std::error::Error>> {
     use std::sync::Arc;
     use zarrs::{
-        array::{DataType, FillValue, ZARR_NAN_F32},
+        array::{DataType, ZARR_NAN_F32},
         array_subset::ArraySubset,
         node::Node,
         storage::store,
@@ -59,8 +59,8 @@ fn array_write_read() -> Result<(), Box<dyn std::error::Error>> {
     let array = zarrs::array::ArrayBuilder::new(
         vec![8, 8], // array shape
         DataType::Float32,
-        vec![4, 4].try_into()?, // regular chunk shape
-        FillValue::from(ZARR_NAN_F32),
+        vec![4, 4], // regular chunk shape
+        ZARR_NAN_F32,
     )
     // .bytes_to_bytes_codecs(vec![]) // uncompressed
     .dimension_names(["y", "x"].into())

zarrs/examples/array_write_read_string.rs
@@ -9,11 +9,7 @@ use zarrs::{
 
 fn array_write_read() -> Result<(), Box<dyn std::error::Error>> {
     use std::sync::Arc;
-    use zarrs::{
-        array::{DataType, FillValue},
-        array_subset::ArraySubset,
-        storage::store,
-    };
+    use zarrs::{array::DataType, array_subset::ArraySubset, storage::store};
 
     // Create a store
     // let path = tempfile::TempDir::new()?;
@@ -59,8 +55,8 @@ fn array_write_read() -> Result<(), Box<dyn std::error::Error>> {
     let array = zarrs::array::ArrayBuilder::new(
         vec![4, 4], // array shape
         DataType::String,
-        vec![2, 2].try_into()?, // regular chunk shape
-        FillValue::from("_"),
+        vec![2, 2], // regular chunk shape
+        "_",
     )
     // .bytes_to_bytes_codecs(vec![]) // uncompressed
     .dimension_names(["y", "x"].into())

zarrs/examples/async_array_write_read.rs
@@ -9,7 +9,7 @@ async fn async_array_write_read() -> Result<(), Box<dyn std::error::Error>> {
     use futures::StreamExt;
     use std::sync::Arc;
     use zarrs::{
-        array::{DataType, FillValue, ZARR_NAN_F32},
+        array::{DataType, ZARR_NAN_F32},
         array_subset::ArraySubset,
         node::Node,
     };
@@ -55,8 +55,8 @@ async fn async_array_write_read() -> Result<(), Box<dyn std::error::Error>> {
     let array = zarrs::array::ArrayBuilder::new(
         vec![8, 8], // array shape
         DataType::Float32,
-        vec![4, 4].try_into()?, // regular chunk shape
-        FillValue::from(ZARR_NAN_F32),
+        vec![4, 4], // regular chunk shape
+        ZARR_NAN_F32,
     )
     // .bytes_to_bytes_codecs(vec![]) // uncompressed
     .dimension_names(["y", "x"].into())

zarrs/examples/custom_data_type_fixed_size.rs
@@ -273,7 +273,7 @@ fn main() {
     let array = ArrayBuilder::new(
         vec![4, 1], // array shape
         DataType::Extension(Arc::new(CustomDataTypeFixedSize)),
-        vec![2, 1].try_into().unwrap(), // regular chunk shape
+        vec![2, 1], // regular chunk shape
         FillValue::new(fill_value.to_ne_bytes().to_vec()),
     )
     .array_to_array_codecs(vec![

commit cc5dd1526c0c5de01ceac51fa13bbf9989874f87

fix: fix benches, broken in #214

zarrs/benches/array_blosc.rs
@@ -1,4 +1,4 @@
-#![allow(missing_docs)]
+//! Benchmark the `blosc` codec in various configurations.
 
 use std::sync::Arc;
 
@@ -16,8 +16,8 @@ fn array_blosc_write_all(c: &mut Criterion) {
                 let store = zarrs::storage::store::MemoryStore::new();
                 let array = zarrs::array::ArrayBuilder::new(
                     vec![size; 3],
-                    zarrs::array::DataType::UInt8,
                     vec![32; 3],
+                    zarrs::array::DataType::UInt8,
                     0u8,
                 )
                 .bytes_to_bytes_codecs(vec![Arc::new(
@@ -51,8 +51,8 @@ fn array_blosc_read_all(c: &mut Criterion) {
             let store = zarrs::storage::store::MemoryStore::new();
             let array = zarrs::array::ArrayBuilder::new(
                 vec![size; 3],
-                zarrs::array::DataType::UInt8,
                 vec![32; 3],
+                zarrs::array::DataType::UInt8,
                 0u8,
             )
             .bytes_to_bytes_codecs(vec![Arc::new(

zarrs/benches/array_subset.rs
@@ -1,6 +1,8 @@
+//! Benchmark the `ArraySubset` indices iterator.
+
 use criterion::{
-    black_box, criterion_group, criterion_main, AxisScale, BenchmarkId, Criterion,
-    PlotConfiguration, Throughput,
+    criterion_group, criterion_main, AxisScale, BenchmarkId, Criterion, PlotConfiguration,
+    Throughput,
 };
 use zarrs::array_subset::ArraySubset;
 
@@ -15,7 +17,7 @@ fn array_subset_indices_iterator(c: &mut Criterion) {
         group.bench_function(BenchmarkId::new("size", array_subset_size), |b| {
             b.iter(|| {
                 array_subset.indices().into_iter().for_each(|indices| {
-                    black_box(indices.first().unwrap());
+                    std::hint::black_box(indices.first().unwrap());
                 })
             });
         });

zarrs/benches/array_uncompressed.rs
@@ -1,3 +1,5 @@
+//! Benchmark uncompressed unsharded and sharded arrays.
+
 use std::sync::Arc;
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
@@ -13,8 +15,8 @@ fn array_write_all(c: &mut Criterion) {
                 let store = zarrs::storage::store::MemoryStore::new();
                 let array = zarrs::array::ArrayBuilder::new(
                     vec![size; 3],
-                    zarrs::array::DataType::UInt8,
                     vec![32; 3],
+                    zarrs::array::DataType::UInt8,
                     0u8,
                 )
                 .build(store.into(), "/")
@@ -40,8 +42,8 @@ fn array_write_all_sharded(c: &mut Criterion) {
                     Arc::new(ShardingCodecBuilder::new(vec![32; 3].try_into().unwrap()).build());
                 let array = zarrs::array::ArrayBuilder::new(
                     vec![size; 3],
-                    zarrs::array::DataType::UInt16,
                     vec![size; 3],
+                    zarrs::array::DataType::UInt16,
                     0u16,
                 )
                 .array_to_bytes_codec(sharding_codec)
@@ -66,8 +68,8 @@ fn array_read_all(c: &mut Criterion) {
             let store = zarrs::storage::store::MemoryStore::new();
             let array = zarrs::array::ArrayBuilder::new(
                 vec![size; 3],
-                zarrs::array::DataType::UInt16,
                 vec![32; 3],
+                zarrs::array::DataType::UInt16,
                 0u16,
             )
             .build(store.into(), "/")
@@ -97,8 +99,8 @@ fn array_read_all_sharded(c: &mut Criterion) {
                 Arc::new(ShardingCodecBuilder::new(vec![32; 3].try_into().unwrap()).build());
             let array = zarrs::array::ArrayBuilder::new(
                 vec![size; 3],
-                zarrs::array::DataType::UInt8,
                 vec![size; 3],
+                zarrs::array::DataType::UInt8,
                 1u8,
             )
             .array_to_bytes_codec(sharding_codec)

zarrs/benches/codecs.rs
@@ -1,3 +1,5 @@
+//! Benchmark various codecs.
+
 use std::borrow::Cow;
 
 use criterion::{

zarrs/benches/fill_value.rs
@@ -1,3 +1,5 @@
+//! Benchmark `FillValue::equas_all()`.
+
 use criterion::{
     criterion_group, criterion_main, AxisScale, BenchmarkId, Criterion, PlotConfiguration,
     Throughput,