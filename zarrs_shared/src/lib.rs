8a175ffcbbc4c0116ff3f06af7e946557d7d1996
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-08-23 13:06:07 -0400

    Update

--- zarrs_shared/src/lib.rs
+++ zarrs_shared/src/lib.rs
@@ -1,9 +1,9 @@
-//! The data type API for the [`zarrs`](https://docs.rs/zarrs/latest/zarrs/index.html) crate.
+//! Shared definitions for multiple [`zarrs`](https://docs.rs/zarrs/latest/zarrs/index.html) workspace crates.
 //!
 //! ## Licence
-//! `zarrs_data_type` is licensed under either of
-//!  - the Apache License, Version 2.0 [LICENSE-APACHE](https://docs.rs/crate/zarrs_data_type/latest/source/LICENCE-APACHE) or <http://www.apache.org/licenses/LICENSE-2.0> or
-//!  - the MIT license [LICENSE-MIT](https://docs.rs/crate/zarrs_data_type/latest/source/LICENCE-MIT) or <http://opensource.org/licenses/MIT>, at your option.
+//! `zarrs_shared` is licensed under either of
+//!  - the Apache License, Version 2.0 [LICENSE-APACHE](https://docs.rs/crate/zarrs_shared/latest/source/LICENCE-APACHE) or <http://www.apache.org/licenses/LICENSE-2.0> or
+//!  - the MIT license [LICENSE-MIT](https://docs.rs/crate/zarrs_shared/latest/source/LICENCE-MIT) or <http://opensource.org/licenses/MIT>, at your option.
 //!
 //! Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
 
