d39d4e1cc87b623a3d624dd5c280bfc4e47eec35
Kyle Barron <kyle@developmentseed.org>
2025-10-09 14:17:39 -0400

    test: Add test with v3 bytes written by Python Zarr

---
+++ zarrs/tests/v3_bytes.rs
@@ -0,0 +1,22 @@
+#![allow(missing_docs)]
+#![cfg(all(feature = "sharding", feature = "zstd"))]
+
+use std::{error::Error, sync::Arc};
+
+use zarrs_filesystem::FilesystemStore;
+
+#[test]
+fn cities_zarr_python_v3_compat() -> Result<(), Box<dyn Error>> {
+    let store = Arc::new(FilesystemStore::new(
+        "tests/data/zarr_python_compat/v3_bytes.zarr",
+    )?);
+    let array = zarrs::array::Array::open(store.clone(), "/")?;
+    let subset_all = array.subset_all();
+    let cities_out = array.retrieve_array_subset_elements::<Vec<u8>>(&subset_all)?;
+
+    assert_eq!(cities_out[0], b"New York");
+    assert_eq!(cities_out[1], b"Los Angeles");
+    assert_eq!(cities_out[2], b"Chicago");
+
+    Ok(())
+}
