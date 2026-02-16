2c95b621dc4e7b0eb27d4abee41e76573ac6a6c1
Lachlan Deakin <ljdgit@gmail.com>
2026-02-17 06:45:00 +1100

    fix: restore `i64` `Element[Owned]` support for `numpy.{datetime64,timedelta64}`
    Fixes #353

--- zarrs/src/array/data_type/numpy_timedelta64.rs
+++ zarrs/src/array/data_type/numpy_timedelta64.rs
@@ -91,6 +91,11 @@ impl zarrs_data_type::DataTypeTraits for NumpyTimeDelta64DataType {
     fn as_any(&self) -> &dyn std::any::Any {
         self
     }
+
+    fn compatible_element_types(&self) -> &'static [std::any::TypeId] {
+        const TYPES: [std::any::TypeId; 1] = [std::any::TypeId::of::<i64>()];
+        &TYPES
+    }
 }
 
 zarrs_data_type::codec_traits::impl_bytes_data_type_traits!(NumpyTimeDelta64DataType, 8);
