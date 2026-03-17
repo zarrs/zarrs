2309d63f2a493fc11125464e1bb45c33e6c5d4ff
ilan-gold <ilanbassgold@gmail.com>
2026-03-17 18:54:19 +0100

    chore: panic doc

--- zarrs/src/array/codec/array_to_bytes/sharding/sharding_codec.rs
+++ zarrs/src/array/codec/array_to_bytes/sharding/sharding_codec.rs
@@ -1093,6 +1093,9 @@ impl ShardingCodec {
     ///
     /// # Errors
     /// Returns [`CodecError`] if the decoded shard index is not valid.
+    /// 
+    /// # Panics
+    /// Panics if the enoded index size or the encoded shard minux its index length exceeds [`usize::MAX`].
     pub fn decode_index(
         &self,
         encoded_shard: &[u8],
