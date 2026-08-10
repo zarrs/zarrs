84b2bf671d2c08812bb1a9d6e75fdf4678897456
Lachlan Deakin <ljdgit@gmail.com>
2026-08-11 00:02:39 +1000

    refactor: add `async_array_read_ops_common`
    Mirror sync

--- zarrs/src/array/array_ops.rs
+++ zarrs/src/array/array_ops.rs
@@ -92,6 +92,8 @@ mod async_array_read_ops;
 #[cfg(feature = "async")]
 mod async_array_read_ops_array;
 #[cfg(feature = "async")]
+mod async_array_read_ops_common;
+#[cfg(feature = "async")]
 mod async_array_update_ops;
 #[cfg(feature = "async")]
 mod async_array_update_ops_array;
